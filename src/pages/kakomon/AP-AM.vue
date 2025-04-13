9880774c3caa072115c25733ec6c39ae724e7ae1
take5054 <78195524+take5054@users.noreply.github.com>
2025-04-13 12:45:26 +0900

    feat: /kakomon/AP-AMに各年度のカードを追加

--- src/pages/kakomon/AP-AM.vue
+++ src/pages/kakomon/AP-AM.vue
@@ -8,12 +8,37 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col>
-
+      <!-- カードがある場合、各カードを横並びに表示 -->
+      <v-col v-for="(card, index) in reversedCardsData" :key="index" cols="4" v-if="cardsData.length > 0">
+        <v-card class="ma-0 pa-4" outlined elevation="6" :to="'/kakomon/AP-AM/' + card[3]">
+          <v-card-title>{{ card[1] }} {{ card[2] === 1 ? '春' : '秋' }}期</v-card-title>
+        </v-card>
+      </v-col>
+      <!-- カードがまだない場合 -->
+      <v-col cols="12" v-else>
+        データを取得中...
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script lang="ts" setup>
+import { ref, computed, onMounted } from 'vue'
+import axios from 'axios'
+import HeaderParallax from '../../components/HeaderParallax.vue'
+
+const cardsData = ref<any[]>([])
+// cardsData の逆順の配列をcomputedで作成
+const reversedCardsData = computed(() => {
+  return [...cardsData.value].reverse()
+})
+
+onMounted(async () => {
+  try {
+    const res = await axios.get("http://localhost:8000/ap-am-index")
+    cardsData.value = res.data
+  } catch (error) {
+    console.error("データ取得エラー", error)
+  }
+})
 </script>
