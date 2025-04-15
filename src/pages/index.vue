c1dd8ac1541fbf83faea5139da600bfdd086038c
take5054 <78195524+take5054@users.noreply.github.com>
2025-04-15 19:33:38 +0900

    feat: 新着情報リストを追加し、更新履歴データを仮実装

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,10 +7,48 @@
         <v-divider :thickness="5" />
       </v-col>
     </v-row>
+    <!-- 新着情報リスト -->
+    <v-row>
+      <v-col>
+        <h2>新着情報</h2>
+        <div class="whats-news">
+          <v-card v-for="(item, index) in updateHistory" :key="index" class="mb-2" outlined>
+            <v-card-title>{{ item.description }}</v-card-title>
+            <v-card-subtitle class="mb-2">更新日 {{ item.date }}</v-card-subtitle>
+          </v-card>
+        </div>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script lang="ts" setup>
-import { ref, onMounted } from 'vue';
+import { ref } from 'vue';
 import HeaderParallax from '@/components/HeaderParallax.vue';
+
+// 更新履歴データ(仮置き・バックエンドから取得するように変更予定)
+const updateHistory = ref([
+  { date: '2025-04-09', description: 'test' },
+  { date: '2025-04-09', description: 'test' },
+  { date: '2025-04-09', description: 'test' },
+  { date: '2025-04-09', description: 'test' },
+  { date: '2025-04-09', description: 'test' },
+  { date: '2025-04-09', description: 'test' },
+  { date: '2025-04-09', description: 'test' },
+  { date: '2025-04-09', description: 'test' },
+  { date: '2025-04-09', description: 'test' },
+  { date: '2025-04-09', description: 'サイトの開発を開始' },
+]);
 </script>
+
+<style scoped>
+.whats-news {
+  max-height: 300px; /* 最大の高さ設定 */
+  overflow-y: auto;
+  padding: 16px;
+  margin-top: 4px;
+  margin-bottom: 4px;
+  border-radius: 5px;
+  background-color: #e9e9e9;
+}
+</style>
